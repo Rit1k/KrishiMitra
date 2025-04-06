bef885e446021ed37cd82d2ce2e317c6432a50f2
surajsinghshekhawat <ss2580@srmist.edu.in>
2025-04-06 21:30:54 +0530

    Update Python version and dependencies for better compatibility

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@ set -o errexit
 
 # Install system dependencies
 apt-get update
-apt-get install -y build-essential python3-dev
+apt-get install -y build-essential python3.8-dev python3.8-venv
+
+# Create and activate virtual environment
+python3.8 -m venv .venv
+source .venv/bin/activate
 
 # Upgrade pip
 pip install --upgrade pip
