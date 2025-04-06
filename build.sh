34c7433d2ef62477473a84f2325a4f2e95788f81
surajsinghshekhawat <ss2580@srmist.edu.in>
2025-04-06 21:20:27 +0530

    Fix build issues with NumPy and add build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install system dependencies
+apt-get update
+apt-get install -y build-essential python3-dev
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install Python packages
+pip install -r requirements.txt 
