7478b80b05bde16e1b838fae9034c64a86d904ad
surajsinghshekhawat <ss2580@srmist.edu.in>
2025-04-06 21:38:26 +0530

    Update to Python 3.11 and NumPy 1.24.3 for better compatibility

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ set -o errexit
 
 # Install system dependencies
 apt-get update
-apt-get install -y build-essential python3.8-dev python3.8-venv
+apt-get install -y build-essential python3.11-dev python3.11-venv
 
 # Create and activate virtual environment
-python3.8 -m venv .venv
+python3.11 -m venv .venv
 source .venv/bin/activate
 
 # Upgrade pip
